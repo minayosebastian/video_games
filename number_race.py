5d2e090a74bf277913569626a074f284d66e3fc0
Sebastian Minayo <minayosebastian2805@gmail.com>
2024-03-18 21:19:48 -0500

    number_race

--- number_race.py
+++ number_race.py
@@ -1,33 +1,67 @@
-from random import randint
+import random
 import os
 
-menu_status = True
-opt_status = True
-#Functions
-def main_menu():
+def clear_screen():
+    os.system('cls' if os.name == 'nt' else 'clear')
 
-    while menu_status:
-        global opt_status
-        global menu_status
-        os.system('clear')
-        print("::: MAIN MENU :::")
-        print("[1]. Play")
-        print("[2]. Help")
-        print("[3]. About us")
-        print("[4]. Exit")
+def lanzar_dados():
+    return random.randint(1, 6), random.randint(1, 6)
 
-    while opt_status:
-        opt = input(".:::Press any option: ")
-        if opt < 1 or opt > 4
-        print(":::Invalid option, try again:::")
-        else:
-            opt_status = False
+def carrera_numerica():
+    print("Bienvenido al juego de Carrera Numérica!")
+    while True:
+        try:
+            num_jugadores = int(input("Ingrese la cantidad de jugadores (entre 2 y 4): "))
+            if num_jugadores < 2 or num_jugadores > 4:
+                raise ValueError("La cantidad de jugadores debe estar entre 2 y 4.")
+            break
+        except ValueError as e:
+            print(e)
     
-    if opt == '4':
-        break
-#Main
-os.system('clear')
-print('Loading...')
-key = input(":::.Press any key to continue.:::")
-main_menu()
+    clear_screen()
 
+    while True:
+        print("Niveles de tablero:")
+        print("1. Nivel básico (Tablero de 20 posiciones)")
+        print("2. Nivel intermedio (Tablero de 30 posiciones)")
+        print("3. Nivel avanzado (Tablero de 50 posiciones)")
+        print("4. Nivel experto (Tablero de 100 posiciones)")
+        nivel = input("Seleccione el nivel de tablero a jugar: ")
+        if nivel in ['1', '2', '3', '4']:
+            if nivel == '1':
+                meta = 20
+            elif nivel == '2':
+                meta = 30
+            elif nivel == '3':
+                meta = 50
+            else:
+                meta = 100
+            break
+        else:
+            print("Por favor, ingrese una opción válida.")
+    
+    clear_screen()
+
+    posiciones = [0] * num_jugadores
+    pares_consecutivos = [0] * num_jugadores
+
+    while True:
+        for jugador in range(num_jugadores):
+            input(f"\nTurno del jugador {jugador + 1}. Presione Enter para lanzar los dados...")
+            dado1, dado2 = lanzar_dados()
+            total = dado1 + dado2
+            nueva_posicion = posiciones[jugador] + total
+            posiciones[jugador] = nueva_posicion
+            print(f"Jugador {jugador + 1}: Has sacado un {dado1} y un {dado2}. Avanzas {total} casillas. Ahora estás en la posición {posiciones[jugador]}")
+
+            if dado1 == dado2:
+                pares_consecutivos[jugador] += 1
+                if pares_consecutivos[jugador] == 3:
+                    print(f"¡Jugador {jugador + 1} ha obtenido 3 pares consecutivos! ¡Ha ganado!")
+                    return
+
+            if nueva_posicion >= meta:
+                print(f"¡Jugador {jugador + 1} ha llegado a la meta y ha ganado!")
+                return
+
+carrera_numerica()
